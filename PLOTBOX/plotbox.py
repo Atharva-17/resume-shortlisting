0a3dd7a2233b30ba116d4b15ca08e979e7036469
pravesh-dholwani <praveshdholwani5@gmail.com>
2022-10-13 19:31:25 +0530

    Updates done to plotbox.py

--- PLOTBOX/plotbox.py
+++ PLOTBOX/plotbox.py
@@ -2,6 +2,7 @@ import cv2
 
 def plotSkills(image , imageOut , skills):
     image = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
     n_boxes = len(imageOut['text'])
     for i in range(n_boxes):
         if  (imageOut['text'][i].lower()) in skills:
